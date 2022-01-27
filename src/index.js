00dbda7719383f4d688f2271d92d2077cefd9259
banlonjones <banlonjones@go-groups.net>
2022-01-27 02:57:22 +0100

    hit the API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,68 @@
 import './style.css';
-import { refresher, render } from './main';
+import { getScores, newScore, refresher } from './main';
+
+const showScore = (score) => `<li> ${score.user} : ${score.score}</li><hr>`;
+
+const render = () => `<header>
+        <div class="app-name">
+            <h1> Leaderboard </h1>
+        </div>
+    </header>
+    <section>
+        <div class="row">
+            <div class="col-6">
+                <div class="card-title">
+                    <div class="title row">
+                        <h2>Recent scores</h2>
+                        <button class="btn btn-default" id="refresh"> Refresh </button>
+                    </div>
+                </div>
+                <div class="shadow">
+                    <ul class="board">
+                    </ul>
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="card-title">
+                    <h2 class="title"> Add your scores </h2>
+                </div>
+                <div class="card-body">
+                    <form name="new-score" id="score-form">
+                        <input type="text" name="user" class="form-control" placeholder="Your name"/>
+                        <input type="number" name="score" class="form-control" placeholder="Your score"/>
+                        <input type="submit" class="btn float" id="submit" value="Submit"/>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>`;
 
 const main = document.querySelector('main');
-main.innerHTML = render;
+main.innerHTML = render();
 
-const refresh = document.querySelector('#refresh');
-refresh.addEventListener('click', () => {
+const refreshBtn = document.getElementById('refresh');
+refreshBtn.addEventListener('click', () => {
   refresher();
+});
+
+const board = document.querySelector('.board');
+const displayBoard = async () => {
+  board.innerHTML = '';
+  const list = await getScores();
+  list.forEach((item) => {
+    board.innerHTML += showScore(item);
+  });
+};
+
+displayBoard();
+
+const newScor = document.getElementById('score-form');
+const { user, score } = newScor.elements;
+newScor.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const response = await newScore(user.value, score.value);
+  if (response === 'Leaderboard score created correctly.') {
+    newScor.reset();
+    displayBoard();
+  }
 });
--- src/main.js
+++ src/main.js
@@ -1,43 +1,29 @@
-export const render = `<header>
-        <div class="app-name">
-            <h1> Leaderboard </h1>
-        </div>
-    </header>
-    <section>
-        <div class="row">
-            <div class="col-6">
-                <div class="card-title">
-                    <div class="title row">
-                        <h2>Recent scores</h2>
-                        <button class="btn btn-default" id="refresh"> Refresh </button>
-                    </div>
-                </div>
-                <div class="shadow">
-                    <ul>
-                        <li> name : 100</li><hr>
-                        <li> name : 100</li><hr>
-                        <li> name : 100</li><hr>
-                        <li> name : 100</li><hr>
-                        <li> name : 100</li><hr>
-                        <li> name : 100</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-6">
-                <div class="card-title">
-                    <h2 class="title"> Add your scores </h2>
-                </div>
-                <div class="card-body">
-                    <form>
-                        <input type="text" class="form-control" placeholder="Your name"/>
-                        <input type="text" class="form-control" placeholder="Your score"/>
-                        <button class="btn float"> Submit</button>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </section>`;
+const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/';
+const apiKey = 'p8t1RkDxhKKQBmrnvQ03';
 
 export const refresher = () => {
   window.location.reload();
+};
+
+export const getScores = async () => {
+  const response = await fetch(`${baseUrl}games/${apiKey}/scores/`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  const { result } = await response.json();
+  return result;
+};
+
+export const newScore = async (user, score) => {
+  const response = await fetch(`${baseUrl}games/${apiKey}/scores/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ user, score }),
+  });
+  const { result } = await response.json();
+  return result;
 };
